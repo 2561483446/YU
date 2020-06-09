919a4356faf81ef11703462aeda4a15cc6c2a84c
MYX <2561483446@qq.com>
2020-06-09 19:48:42 +0800

    Default Changelist

---
+++ src/main/java/com/servlet/LoginActionServlet.java
@@ -0,0 +1,69 @@
+package com.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+public class LoginActionServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+
+        Connection connection = null;
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/s82","root","123456");
+            String sql = "select username , password from user where username = ? and password";
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setObject(1,username);
+            ps.setObject(2,password);
+
+            ResultSet rs = ps.executeQuery();
+
+            PrintWriter pw = response.getWriter();
+            if (rs.next()){
+                pw.println("yes");
+            }else {
+                pw.println("no");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
